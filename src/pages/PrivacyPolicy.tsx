c74e4e05d88977c21cfd860238bfb96fbae3c368
BeLieVer031 <sandyrajak031@gmail.com>
2025-06-05 22:25:54 +0530

    fix: h2 color fix

--- src/pages/PrivacyPolicy.tsx
+++ src/pages/PrivacyPolicy.tsx
@@ -3,20 +3,20 @@ function PrivacyPolicy() {
     return (
         <div className="w-1/2 mx-auto pt-10 space-y-4">
             <h1 className="text-5xl">Privacy Policy</h1>
-            <h2 className="text-3xl text-neutral-700">Who we are</h2>
+            <h2 className="text-3xl">Who we are</h2>
             <p className="text-2xl text-neutral-600">Our website address is: https://habitrobotics.com.</p>
-            <h2 className="text-3xl text-neutral-700">Comments</h2>
+            <h2 className="text-3xl">Comments</h2>
             <p className="text-2xl text-neutral-600">
                 When visitors leave comments on the site we collect the data shown in the comments form, and also the visitor’s IP address and browser user agent string to help spam detection.
             </p>
             <p className="text-2xl text-neutral-600">
                 An anonymized string created from your email address (also called a hash) may be provided to the Gravatar service to see if you are using it. The Gravatar service privacy policy is available here: https://automattic.com/privacy/. After approval of your comment, your profile picture is visible to the public in the context of your comment.
             </p>
-            <h2 className="text-3xl text-neutral-700">Media</h2>
+            <h2 className="text-3xl">Media</h2>
             <p className="text-2xl text-neutral-600">
                 If you upload images to the website, you should avoid uploading images with embedded location data (EXIF GPS) included. Visitors to the website can download and extract any location data from images on the website.
             </p>
-            <h2 className="text-3xl text-neutral-700">Cookies</h2>
+            <h2 className="text-3xl">Cookies</h2>
 
             <p className="text-2xl text-neutral-600">
 
@@ -37,7 +37,7 @@ function PrivacyPolicy() {
             </p>
 
 
-            <h2 className="text-3xl text-neutral-700">
+            <h2 className="text-3xl">
                 Embedded content from other websites
             </h2>
 
@@ -49,7 +49,7 @@ function PrivacyPolicy() {
                 These websites may collect data about you, use cookies, embed additional third-party tracking, and monitor your interaction with that embedded content, including tracking your interaction with the embedded content if you have an account and are logged in to that website.
             </p>
 
-            <h2 className="text-3xl text-neutral-700">
+            <h2 className="text-3xl">
                 Who we share your data with
             </h2>
 
@@ -58,7 +58,7 @@ function PrivacyPolicy() {
             </p>
 
 
-            <h2 className="text-3xl text-neutral-700">
+            <h2 className="text-3xl">
 
 
                 How long we retain your data
@@ -72,7 +72,7 @@ function PrivacyPolicy() {
             <p className="text-2xl text-neutral-600">
                 For users that register on our website (if any), we also store the personal information they provide in their user profile. All users can see, edit, or delete their personal information at any time (except they cannot change their username). Website administrators can also see and edit that information.
             </p>
-            <h2 className="text-3xl text-neutral-700">
+            <h2 className="text-3xl">
                 What rights you have over your data
             </h2>
 
@@ -80,7 +80,7 @@ function PrivacyPolicy() {
                 If you have an account on this site, or have left comments, you can request to receive an exported file of the personal data we hold about you, including any data you have provided to us. You can also request that we erase any personal data we hold about you. This does not include any data we are obliged to keep for administrative, legal, or security purposes.
             </p>
 
-            <h2 className="text-3xl text-neutral-700">
+            <h2 className="text-3xl">
                 Where your data is sent
             </h2>
 
