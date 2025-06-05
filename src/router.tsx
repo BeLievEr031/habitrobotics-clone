e80e301811671c562df01c6300033c24b79c4072
BeLieVer031 <sandyrajak031@gmail.com>
2025-06-05 22:23:45 +0530

    feat: add privacy policy page

--- src/router.tsx
+++ src/router.tsx
@@ -1,6 +1,7 @@
 import { createBrowserRouter } from "react-router-dom";
 import RootLayout from "./layouts/RootLayout";
 import Home from "./pages/Home";
+import PrivacyPolicy from "./pages/PrivacyPolicy";
 
 const router = createBrowserRouter([
     {
@@ -10,7 +11,11 @@ const router = createBrowserRouter([
             {
                 path: "",
                 element: <Home />
-            }
+            },
+            {
+                path: "privacy-policy",
+                element: <PrivacyPolicy />
+            },
         ]
     }
 ])
