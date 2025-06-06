9b7b9a275a4cad90a47e64655ca8cdc69fe98b25
BeLieVer031 <sandyrajak031@gmail.com>
2025-06-06 12:12:37 +0530

    feat: add mobile nav

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,7 +1,9 @@
-import { LucideMenu } from "lucide-react"
+import { LucideMenu, X } from "lucide-react"
 import { Link } from "react-router-dom"
 import clsx from "clsx";
+import { useState } from "react";
 function Navbar() {
+    const [toggle, setToggle] = useState(false)
     return (
         <div className="container py-4 flex justify-between items-center w-11/12 mx-auto">
 
@@ -20,7 +22,28 @@ function Navbar() {
                 >carrers</Link>
             </div>
 
-            <LucideMenu className="block sm:hidden" />
+            <LucideMenu className="block sm:hidden cursor-pointer" onClick={() => setToggle(!toggle)} />
+
+
+            {toggle && <div className={clsx("absolute bg-white w-full h-full z-50 top-0 left-0  px-4 pt-4")}>
+                <div className="flex justify-end cursor-pointer">
+                    <X onClick={() => setToggle(!toggle)} />
+                </div>
+                <div className="flex flex-col justify-end items-end py-2 space-y-3">
+                    <Link to={"/"} className={clsx(
+                        "font-light text-xl"
+                    )}
+                        onClick={() => setToggle(!toggle)}
+                    >home</Link>
+
+                    <Link to={"/careers"}
+                        className={clsx(
+                            "font-light text-xl"
+                        )}
+                        onClick={() => setToggle(!toggle)}
+                    >carrers</Link>
+                </div>
+            </div>}
         </div >
     )
 }
